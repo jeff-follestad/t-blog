9d689553a1ba6a18e3b94ab01116edd03a1f85fe
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-06-27 14:51:29 -0700

    can't get the database to work in Heroku, works locally

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'top secret',
+  secret: 'bigbluedog',
   cookie: {
         // Session will automatically expire in 10 minutes
         expires: 10 * 60 * 1000
